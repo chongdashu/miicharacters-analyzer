f611635b2362d5692278a686c0b1839749293bed
Chong-U <culim@mit.edu>
2016-02-04 16:41:02 -0500

    Update to fix script

--- scrape-miis.sh
+++ scrape-miis.sh
@@ -1,12 +1,12 @@
-for i in 1 {0..10}
-do
-    t=$(( ( RANDOM % 3 )  + 1 ))
+in {1..2847}
+    do
+t=$(( ( RANDOM % 5 )  + 1 ))
     echo Sleeping for $t
 
     sleep ${t}s
-    m=i%10
+m=$((i%10))
     if [ ! -d html/$m/$i ]; then
-        mkdir -p html/$m
+    mkdir -p html/$m
     fi;
-    # wget http://www.miicharacters.com/?mii=${i} -O html/$m/${i}.html
-done
+    wget http://www.miicharacters.com/?mii=${i} -O html/$m/${i}.html
+    done
